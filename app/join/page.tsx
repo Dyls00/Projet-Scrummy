79dce901e4002c473c8b924175658ebe3243dc2e
johane <nsangouakandajohane@gmail.com>
2025-06-19 22:53:33 +0200

    nouvelle mof

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -1,7 +1,7 @@
 export default function Join() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
-        <p>Nous ici</p>
+        <p>This is the pages section of the Join</p>
     </div>
   );
 }
--- app/lib/page.tsx
+++ app/lib/page.tsx
@@ -1,7 +1,7 @@
-export default function CreatePage() {
+export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
-        <p>This is the pages section of the create</p>
+      <p>lib</p>
     </div>
   );
 }

06b0c34f72afaad0adc3a9c4a90dbfc09aa7aa1b
johane <nsangouakandajohane@gmail.com>
2025-06-19 22:48:41 +0200

    nouveau

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,7 +1,7 @@
 export default function CreatePage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
-        <p>Pourquoi seulement lui</p>
+        <p>Pourquoi seulement lui;;</p>
     </div>
   );
 }
---
+++ app/dashboard/pages.tsx
@@ -0,0 +1,8 @@
+export default function Pages() {
+  return (
+    <div>
+      <h1>Pages</h1>
+      <p>This is the pages section of the dashboard.</p>
+    </div>
+  );
+}
